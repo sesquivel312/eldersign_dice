ac867c007c6b075386a46f1d5f678dc8eb09da5a
nunya <nunya@nunya.com>
2020-11-08 19:48:03 -0700

    begin accounting for classes of sides and counting appropriately

--- eldersign_dice.py
+++ eldersign_dice.py
@@ -31,6 +31,7 @@ to expand to take account of the following:
     . there are some dice with different sides and one has a "wild"
 
 """
+from collections import Counter
 import math
 import random
 
@@ -38,22 +39,23 @@ import random
 # investigate-1,  investigate-2, investigate-3, peril, terror, lore
 # lets' call that 1, 2, 3, 4, 5, 6?
 
-die = [1,2,3,4,5,6]
+die = [1, 2, 3, 4, 5, 6]
 faces = {'investigate1': 1, 'investigate2': 2, 'investigate3': 3, 'peril': 4, 'terror': 5, 'lore': 6}
 
 
-def roll(pool_sz):
+def roll_pool(die, pool_sz):
     """
     roll a pool of six dice, return as a list
 
     todo generalize for arbitrary die size
+    :param die:
     :param pool_sz:
     :return:
     """
     return [random.choice(die) for _ in range(pool_sz)]
 
 
-def count_sides(roll, sides):
+def count_results(roll, sides):
     """
     count the number of matching sides in a given roll of some number of dice
 
@@ -65,11 +67,16 @@ def count_sides(roll, sides):
 
     roll.sort()
 
-    # extract only die results matching the side of interest - i.e. filtered
+    # extract only die results matching the sides of interest - i.e. filtered
     # is a list where all elements are the same value (or there aren't any elements)
     # the length of filtered is the number of die results matching side
-    filtered = [e for e in roll if e in sides]
-    return len(filtered)
+    results = {}
+
+    for side in sides:
+        filtered = [e for e in roll if e == side]
+        results[side] = len(filtered)
+
+    return results
 
 
 def get_hist(pool_sz=6, num_rolls=100, sides=[6]):
@@ -84,17 +91,28 @@ def get_hist(pool_sz=6, num_rolls=100, sides=[6]):
     """
 
     # initialize the histogram, hist[i] = number of times a count of i came up, which can be 0 so + 1
-    hist = [0 for _ in range(pool_sz + 1)]
+    # hist = [0 for _ in range(pool_sz + 1)]
+    # create a dict whose keys are from sides, the values are set to 0 and will become the counts for each side
+    # todo fix this, the counter actually needs all possible values, plus 0, as it did when I used a list
+    # this is only getting one count, for 6 for example, adding up all the 6's and dividing by the total rolls
+    # but, what I want to know is how many  times did I get 0, 1, 2, ..., 6 6s in the number of rolls requested
+    # I think this is close - I still need the key for 0 "sides"
+    d = {k: 0 for k in die}
+    d[0] = 0
+    hist = Counter(d)  # creates a counter, which uses a dict to count things, which are the keys of the counter
 
-    sides.sort()
+    # sides.sort()
 
     for i in range(num_rolls):
-        r = roll(pool_sz)
-        c = count_sides(r, sides)
-        # print(c, r)
-        hist[c] += 1
+        roll = roll_pool(die=die, pool_sz=pool_sz)
+        count = count_results(roll, sides)
+        print(f'Roll {i}: ', count, roll)
+        # hist[count] += 1
+        hist.update(count)
 
-    return [(100 * e)/num_rolls for e in hist]  # get pct
+    # print('final hist: ', dict(hist))
+    # return [(100 * e) / num_rolls for e in hist]  # get pct
+    return {k: v/num_rolls for k, v in hist.items()}
 
 
-print(get_hist(pool_sz=8, num_rolls=10000, sides=[5,6]))
+get_hist(pool_sz=6, num_rolls=10, sides=[6])
